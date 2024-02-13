67b1bfee1196547a0cfe691503c33f77e8aec52d
Erick-William <erick.william@lekto.com.br>
2024-02-13 11:30:47 -0300

    created project login screen

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 100%;
+  height: 42px;
+  background-color: #81259d;
+  color: #fff;
+
+  border: 1px solid #81259d;
+  border-radius: 21px;
+
+  &:hover {
+    ${({ disabled }) => !disabled && `opacity: 0.6;cursor: pointer;`}
+  }
+
+  ${({disabled}) => disabled && ` opacity: 0.5; cursor: not-allowed;`}
+`;
